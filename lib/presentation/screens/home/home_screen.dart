bff95bdfa3c44cc6d75f33fc0d61d2be3d860773
Uriel Cisneros <urielcisne96@gmail.com>
2023-09-07 20:24:06 -0600

    Change the config route, add library go_router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -50,13 +51,7 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-        //TODO: Navigate to others views
-        // Navigator.of(context).push(
-        //   MaterialPageRoute(
-        //     builder: (context) => const ButtonsScreen(),
-        //   ),
-        // );
-        Navigator.pushNamed(context, menuItem.link);
+        context.go(menuItem.link);
       },
     );
   }
